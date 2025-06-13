338a847889038ec9c7dccc9f5220a54c772f4771
Lokesh Umredkar <lokeshumredkar2003@gmail.com>
2025-06-14 01:29:35 +0530

    updation

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from app import create_app
 from flask_cors import CORS
 from flask_compress import Compress
-
+import os
 app = create_app()
 Compress(app)
 
 CORS(app)
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", debug=True, port=port)
